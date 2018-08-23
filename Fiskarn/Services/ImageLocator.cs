8ce74fe4bbd5e360065ca6d58ec6a63cacb2df86
henke164 <henrik.mattsson@hotmail.com>
2018-08-23 19:50:14 +0200

    sensitivity setting

--- Fiskarn/Services/ImageLocator.cs
+++ Fiskarn/Services/ImageLocator.cs
@@ -1,4 +1,6 @@
 ﻿using AForge.Imaging;
+using System;
+using System.Configuration;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Linq;
@@ -7,9 +9,18 @@ namespace Fiskarn.Services
 {
     public class ImageLocator
     {
+        private readonly float _sensitivity = 0.9f;
+
+        public ImageLocator()
+        {
+            var sensitivity = ConfigurationSettings.AppSettings.Get("sensitivity").Replace('.', ',');
+            _sensitivity = float.Parse(sensitivity);
+            Console.WriteLine($"Sensitivity: {_sensitivity}.");
+        }
+
         public Point FindInImage(Bitmap template, Bitmap sourceImage)
         {
-            var tm = new ExhaustiveTemplateMatching(0.9f);
+            var tm = new ExhaustiveTemplateMatching(_sensitivity);
 
             var matchings = tm.ProcessImage(sourceImage, template);
 
